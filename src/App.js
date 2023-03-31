2140bfe5fdf42681806bb765a0f7a6074f1bff73
ajits731 <ajits731@gmail.com>
2023-03-31 23:10:26 +0530

    Week28-day4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import FunctionalComponent from './components/functionComponent';
-import ListsComponent from './components/ListsAndKeys';
-import RenderComponent from './components/conditionRendering';
-import PureComponent from './components/pureComponent';
-import ClassComponent from './components/classComponent';
+import {createBrowserRouter, RouterProvider, Route, createRoutesFromElements} from 'react-router-dom';
+import Home from './components/home';
+import Navigation from './common/navbar';
+import ProductDetails from './common/productDetails';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+   <Route path='/' element={<Navigation />} >
+     <Route index element={<Home />} />
+     <Route path={"/product/:productid"} element={<ProductDetails />} />
+   </Route> 
+  )
+)
+
 
 function App() {
-  
   return (
-    <div className="App">
-      <ClassComponent />
-      {/* <FunctionalComponent /> */}
-      {/* <ListsComponent /> */}
-      {/* <RenderComponent /> */}
-      {/* <PureComponent elements={[3,2]} /> */}
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import ClassComponent from './components/classComponent';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hi Router is working <a href='/class'>Go to Class</a></div>
-  },
-  {
-    path: '/class',
-    element: <ClassComponent />
-  }
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
